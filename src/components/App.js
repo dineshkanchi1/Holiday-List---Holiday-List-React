798dd76553cca813b856b4720907d15a054f383f
k.d.v.kumar123@gmail.com <k.d.v.kumar123@gmail.com>
2020-10-06 14:35:48 +0530

    commied

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,36 @@ import '../styles/App.css';
 class App extends Component {
     render() {
 
+        const locations = [
+            {
+                city:"Goa",
+                country:"India"
+            },
+            {
+                city:"Amsterdam",
+                country:"Netherlands"
+            },
+            {
+                city:"New York",
+                country:"USA"
+            },
+            {
+                city:"Darjeeling",
+                country:"India"
+            },
+            {
+                city:"Tokyo",
+                country:"Japan"
+            },
+            {
+                city:"Lonavala",
+                country:"India"
+            },
+        ]
         return(
            <>
-//write your code here
-	</>
+            {locations.filter((el) => el.country==="India").map((el)=>(<h1 key={el.city}>{el.city}</h1>))}
+	    </>
         )
     }
 }
