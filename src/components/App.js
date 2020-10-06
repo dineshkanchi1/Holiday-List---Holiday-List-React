234a03f0d1d5269af5b8ab159b12af77332c756d
k.d.v.kumar123@gmail.com <k.d.v.kumar123@gmail.com>
2020-10-06 15:00:18 +0530

    commied

--- src/components/App.js
+++ src/components/App.js
@@ -4,42 +4,46 @@ import '../styles/App.css';
 class App extends Component {
     render() {
 
-        const locations = [
+        let locations = [
             {
-                city:"Goa",
-                country:"India"
+              city: "Goa",
+              country: "India"
             },
             {
-                city:"Amsterdam",
-                country:"Netherlands"
+              city: "Amsterdam",
+              country: "Netherlands"
             },
             {
-                city:"New York",
-                country:"USA"
+              city: "New York",
+              country: "USA"
             },
             {
-                city:"Darjeeling",
-                country:"India"
+              city: "Darjeeling",
+              country: "India"
             },
             {
-                city:"Tokyo",
-                country:"Japan"
+              city: "Tokyo",
+              country: "Japan"
             },
             {
-                city:"Lonavala",
-                country:"India"
-            },
-        ]
-        return(
-           <>
-            {
-            let indian=locations.filter((el) => el.country==="India");
-            indian.map((el,index)=>(<h1 key={index+1}>{el.city}</h1>));
-            let others=locations.filter((el) => el.country!=="India");
-            others.map((el,index)=>(<h1 key={indian.length+index+1}>{el.city}</h1>))
+              city: "Lonavala",
+              country: "India"
             }
-	    </>
-        )
+          ];
+        
+          let indiaCities = locations.filter((el) => el.country === "India");
+          return (
+            <>
+              {indiaCities.map((el, index) => (
+                <h1 key={"location" + (index + 1)}>{el.city}</h1>
+              ))}
+              {locations
+                .filter((el) => el.country !== "India")
+                .map((el, index) => (
+                  <h1 key={"location" + (indiaCities.length + index + 1)}>{el.city}</h1>
+                ))}
+            </>
+          );
     }
 }
 
